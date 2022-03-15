977684f730463a6030121695073595cc7676b6c1
jacobnzala <nzalajacob@gmail.com>
2022-03-15 10:57:33 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * printing the alphabet and then a new line
+ * Return: Always success
+ */
+void print_alphabet(void)
+{
+	char = j;
+	j = 'a';
+	while (j  <= 'z')
+		{
+			putchar(j);
+			j++;
+		}
+	putchar('\n');
+}
+
+
