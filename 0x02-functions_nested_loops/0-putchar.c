19afbf2d842102d1bfc086064e5752b5374106bc
jacobnzala <nzalajacob@gmail.com>
2022-03-15 05:02:23 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <main.h>
+
+/**
+ * main - print -putchar, followed by a new line
+ * Return: Always 0 (sucess)
+ */
+
+
+
+
 int main(void)
 {       putchar(-);
 	putchar(p);
@@ -11,5 +20,5 @@ int main(void)
 	putchar(a);
 	putchar(r);
 	putchar(\n);
-   return(0);
+        return(0);
 }
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+functions and nested loops are covered here
