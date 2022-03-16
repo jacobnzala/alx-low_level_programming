ae2c54b2a7b72670512cc29e4fdef8d7b9ef11de
jacobnzala <nzalajacob@gmail.com>
2022-03-16 09:59:27 -0700

    main

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,5 @@
+#include "main.h"
+
+/**
+ * write a function that prints the sign of a number
+ *
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,28 +1,10 @@
-#include "main.h"
-
-/**
- * print the alphabet times 10
- * Return :0 for now
- */
-
-int print_alphabet_x10(void)
-{
-	char j;
-	
-	t = 0;
-
-	while (t < 10){
-		j = 'a';
-		while (j <= 'z')
-		{
-			putchar(j);
-			j++;
-		}
-
-		putchar('\n');
-		t++;
-	}
-}
-		
-
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abc(int);
 
