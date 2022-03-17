20680e2efc6ce507df4d430ae532b3ca010e2539
jacobnzala <nzalajacob@gmail.com>
2022-03-17 13:23:58 -0700

    1 main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 
 /**
@@ -10,7 +11,7 @@ int main(void)
 {
 	int i;
 
-	printf("Infinite loop incoming :(\n)");
+	printf("Infinite loop incoming :(\n");
 
 	i = 0;
 
