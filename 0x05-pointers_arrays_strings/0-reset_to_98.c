372402c16010768862e32e1375eb0471d4d4ed36
jacobnzala <63255452+jacobnzala@users.noreply.github.com>
2022-03-23 11:59:46 +0200

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,59 +1,10 @@
 #include "main.h"
-
-
-
 /**
- *
- *  * _strcat - concatenates two strings
- *
- *   * @dest: string to append to
- *
- *    * @src: string to add
- *
- *     *
- *
- *      * Return: a pointer to the resulting string
- *
- *       */
-
-char *_strcat(char *dest, char *src)
-
+ * reset_to_98 - check the code for Holberton School students.
+ * @n: integer
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
 {
-
-		int i, j;
-
-
-
-			i = 0;
-
-				j = 0;
-
-
-
-					while (dest[i] != '\0')
-
-								i++;
-
-
-
-						while (src[j] != '\0')
-
-								{
-
-											dest[i] = src[j];
-
-													j++;
-
-															i++;
-
-																}
-
-
-
-							dest[i] = '\0';
-
-
-
-								return (dest);
-
+*n = 98;
 }
