076b63d04a9612c368c9da0a73497ed5d4f1bb3b
jacobnzala <nzalajacob@gmail.com>
2022-03-23 03:34:47 -0700

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ *
+ * * print_array -change value using the pointer address
+ *
+ * *
+ *
+ * *@a: stores the input
+ *
+ * *@n: integer
+ *
+ * */
+
+void print_array(int *a, int n)
+
+{
+
+	int i;
+
+	for (i = 0; i < n; i++)
+
+	{
+
+		printf("%d", a[i]);
+
+		if (i != (n - 1))
+
+		{
+
+			printf(", ");
+
+		}
+
+	}
+
+	printf("\n");
+
+}
