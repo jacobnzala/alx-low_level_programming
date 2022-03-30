363d776248eaf011ba821d6d8fe9879604833c0f
jacobnzala <63255452+jacobnzala@users.noreply.github.com>
2022-03-30 18:28:13 +0200

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,24 +1,13 @@
 #include "main.h"
 /**
- *
- *  *_strlen_recursion - check the code.
- *
- *   *@s: character
- *
- *    * Return: Always 0.
- *
- *     */
-
+ *_strlen_recursion - check the code.
+ *@s: character
+ * Return: Always 0.
+ */
 int _strlen_recursion(char *s)
-
 {
-
-	if (*s == '\0')
-
-		return (0);
-
-	else
-
-		return (1 + _strlen_recursion(s + 1));
-
+if (*s == '\0')
+return (0);
+else
+return (1 + _strlen_recursion(s + 1));
 }
