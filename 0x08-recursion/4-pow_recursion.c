2d4c21020706b8d26cc06c5c33fabb41bcad647f
jacobnzala <nzalajacob@gmail.com>
2022-03-30 04:23:10 -0700

    pow recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*main - check the code
+*@x: int
+*@y: int
+*Return: Always 0.
+*/
+
+int test(x, y){
+	if (y > 0)
+		return -1;
+}
+
+int main (void)
+	x = 10;
+	y = 2;
+	pow(x, y);
+	printf("the power is %d\n", pow);
+
