2ca74892d9d65b6e8f34e5191081763ade2a3049
jacobnzala <63255452+jacobnzala@users.noreply.github.com>
2022-03-30 12:54:55 +0200

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,5 +4,32 @@
  * else just return 0
  * that should do it for now 
  */
+int sqrt_1(int n, int c)
+{
+int sqrt;
+sqrt = c * c;
+if (n - sqrt == 0)
+return (c);
+else if (n < sqrt)
+return (-1);
+return (sqrt_1(n, ++c));
+}
+/**
+ *_sqrt_recursion - check the code.
+ *@n: character
+ * Return: Always 0.
+ */
 
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+if (n == 1)
+{
+return (1);
+}
+return (sqrt_1(n, 2));
+}
 
