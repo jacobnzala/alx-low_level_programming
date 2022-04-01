aef026e80fc912f12131011310237d250af48769
jacobnzala <63255452+jacobnzala@users.noreply.github.com>
2022-04-01 22:29:08 +0200

    Update main.h

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,4 +1,7 @@
-int _putchar(char c);
-int main(int argc, char *argv[]);
-int checkString(char *Str);
-
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
