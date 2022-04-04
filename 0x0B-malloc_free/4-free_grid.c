c06797684889be27c21f541e710ea8a0cbbb78bc
jacobnzala <63255452+jacobnzala@users.noreply.github.com>
2022-04-04 22:50:13 +0200

    Update 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,3 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+* free_grid - free matrix pointer
+* @grid: matrix
+* @height: height
+*Return: pointer to the matrix
+*/
+void free_grid(int **grid, int height)
+{
+int i;
+for (i = 0; i < height; ++i)
+{
+free(grid[i]);
+}
+free(grid);
+}
