3508d5ef68411d3371a63c80e688a41c188fa8d3
jacobnzala <63255452+jacobnzala@users.noreply.github.com>
2022-04-04 22:46:46 +0200

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,28 +1,36 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * strdup (string)
- * return a pointer to a duplicate string
- *
- */
-
-int main (void)
+* _strdup - point new allocated pointer
+* @str: string
+* Return: pointer
+*/
+char *_strdup(char *str)
 {
-	char jacob1[] = "Holberton School!";
-	char *p;
-	p= strdup (jacob1);
-
-	if (p == NULL)
-	{
-		        putchar("failed to allocate memory\n");
-		        return (0);
-
-				    }
-
-	    printf("%s\n", s);
-	    free(s);
-	    return (0);
-
+int str_size;
+int i;
+char *p;
+str_size = 0;
+if (str == 0)
+return (0);
+while (str[str_size] != '\0')
+{
+str_size++;
+}
+str_size++;
+p = malloc(str_size *sizeof(char));
+if (p == 0)
+return (0);
+i = 0;
+while (str[i] != '\0')
+{
+p[i] = str[i];
+i++;
+}
+p[i] = '\0';
+return (p);
 }
