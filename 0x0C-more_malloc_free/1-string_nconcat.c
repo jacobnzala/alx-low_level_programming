efba88fc2c72588700e4c1eb509a6f191821556a
jacobnzala <63255452+jacobnzala@users.noreply.github.com>
2022-04-07 18:42:47 +0200

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,51 +1,45 @@
-#include "main.h"
 #include <stdlib.h>
-/**
- * _strlen - length of a string
- * @s: input char
- * Return: length of a string
- */
+#include "main.h"
 
-int _strlen(char *s)
-{
-int i;
-i = 0;
-while (s[i] != '\0')
-i++;
-return (i);
-}
 /**
-*string_nconcat - change value using
-*@s1: manipulates char1
-*@s2: manipulates char2
-*@n: manipulates integer
-*Return: memory
-*/
+ * *string_nconcat - concatenates n bytes of a string to another string
+ * @s1: string to append to
+ * @s2: string to concatenate from
+ * @n: number of bytes from s2 to concatenate to s1
+ *
+ * Return: pointer to the resulting string
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int a, b, i;
-char *p;
-if (!s1)
-s1 = "";
-else
-a = _strlen(s1);
-if (!s2)
-s2 = "";
-else
-b = _strlen(s2);
-if (n > b)
-n = b;
-p = malloc((a + n + 1) * sizeof(char));
-if (p == NULL)
-return (NULL);
-for (i = 0; i < a; i++)
-{
-p[i] = s1[i];
-}
-for (; i < (a + n); i++)
-{
-p[i] = s2[i - a];
-}
-p[i] = '\0';
-return (p);
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (!s)
+		return (NULL);
+
+	while (i < len1)
+	{
+		s[i] = s1[i];
+		i++;
+	}
+
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+
+	s[i] = '\0';
+
+	return (s);
 }
