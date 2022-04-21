645364c1f6116c9ebb7222da6935046f8cb89f4d
jacobnzala <63255452+jacobnzala@users.noreply.github.com>
2022-04-21 14:05:04 +0200

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,15 +2,10 @@
 
 #include "lists.h"
 
-
-
 /**
- *
- *  * free_list - frees a linked list
- *
- *   * @head: list_t list to be freed
- *
- *    */
+ * free_list - frees a linked list
+ * @head: list_t list to be freed
+ */
 
 void free_list(list_t *head)
 
