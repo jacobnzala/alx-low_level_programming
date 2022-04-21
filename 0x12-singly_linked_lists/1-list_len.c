62d0ef9ff90f8818b9e40996b778b684c716b2f0
jacobnzala <63255452+jacobnzala@users.noreply.github.com>
2022-04-21 14:16:47 +0200

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,11 +7,12 @@ i#include "lists.h"
 
 size_t list_len(const list_t *h)
 {
-int i = 0;
-while (h)
-{
-h = h->next;
-i++;
-}
-return (i);
+	size_t n = 0;
+
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
 }
