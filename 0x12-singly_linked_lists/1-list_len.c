ad5f7b2ace8167005a652b3bf25d010277da4a77
jacobnzala <63255452+jacobnzala@users.noreply.github.com>
2022-04-21 14:08:21 +0200

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,14 +1,9 @@
 i#include "lists.h"
-
 /**
- *
- * * list_len - prints list
- *
- * * @h: pointer
- *
- * *Return: pointer
- *
- * */
+* list_len - prints list
+* @h: pointer
+*Return: pointer
+*/
 
 size_t list_len(const list_t *h)
 
