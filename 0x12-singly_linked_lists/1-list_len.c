baecb5f646c2d96c350efc089d3b93adb7df83bc
jacobnzala <nzalajacob@gmail.com>
2022-04-21 03:40:39 -0700

    list len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,31 @@
+i#include "lists.h"
+
+/**
+ *
+ * * list_len - prints list
+ *
+ * * @h: pointer
+ *
+ * *Return: pointer
+ *
+ * */
+
+size_t list_len(const list_t *h)
+
+{
+
+	int i = 0;
+
+	while (h)
+
+	{
+
+		h = h->next;
+
+		i++;
+
+	}
+
+	return (i);
+
+}
