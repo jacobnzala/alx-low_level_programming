dfc70d1ad70db8cbfd9f5262ddcb2a25462147ab
jacobnzala <63255452+jacobnzala@users.noreply.github.com>
2022-04-25 13:48:12 +0200

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,1 +1,21 @@
-#include "main.h"
+#include "lists.h"
+/**
+* free_listint - free list
+* @head: pointer
+*Return: pointer
+*/
+void free_listint(listint_t *head)
+{
+listint_t *val = head;
+if (val)
+{
+while (val->next)
+{
+head = val;
+val = val->next;
+free(head);
+}
+free(val);
+}
+}
+© 2022 GitHub, Inc.
