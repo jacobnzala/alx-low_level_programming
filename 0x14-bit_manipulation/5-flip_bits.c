9751bd61001a01fad34fe3fca711a671228ad07e
jacobnzala <63255452+jacobnzala@users.noreply.github.com>
2022-04-28 20:40:47 +0200

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,2 +1,29 @@
 #include "main.h"
-
+#include <stdlib.h>
+/**
+ * countSetBits - counter
+ * @n: containing the number
+ * Return: the converted number
+ */
+int countSetBits(unsigned long int n)
+{
+unsigned long int count = 0;
+while (n > 0)
+{
+count++;
+n &= (n - 1);
+}
+if (count > sizeof(n) * 8)
+return (-1);
+return (count);
+}
+/**
+ * flip_bits - flips index to x0r
+ * @n: containing the number
+ * @m: containing the number
+ * Return: the converted number
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+return (countSetBits(n ^ m));
+}
