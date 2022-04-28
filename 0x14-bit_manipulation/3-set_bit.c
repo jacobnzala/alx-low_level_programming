4e651881035c20d125d9f4c98a8805bcc24b3f50
jacobnzala <63255452+jacobnzala@users.noreply.github.com>
2022-04-28 20:39:21 +0200

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,2 +1,15 @@
 #include "main.h"
-
+#include <stdlib.h>
+/**
+ * set_bit - gets int to index number
+ * @n: containing the decimal number
+ * @index: containing the index
+ * Return: the converted number
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+if (index > sizeof(n) * 8)
+return (-1);
+*n |= (1 << index);
+return (1);
+}
